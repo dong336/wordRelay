c522b136fd31e743823059c6e3a62639fa1d51c3
kdw <50583568+dong336@users.noreply.github.com>
2024-06-28 14:29:07 +0900

    Initial Commit

---
+++ wordRelay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wordRelay
+//
+//  Created by 김동운 on 2024/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wordRelay/wordRelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordRelayApp.swift
+//  wordRelay
+//
+//  Created by 김동운 on 2024/06/28.
+//
+
+import SwiftUI
+
+@main
+struct wordRelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
